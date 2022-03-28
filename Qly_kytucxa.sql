f9a9b9bbfdf6cc84f317c3ec5bfc3a93549c0f80
NguyenHieu-1tap <nguyenhieuxyz159@gmail.com>
2022-03-28 21:29:50 +0700

    Update all

--- Qly_kytucxa.sql
+++ Qly_kytucxa.sql
@@ -1,92 +1,48 @@
 create database Qly_kytucxa;
 use Qly_kytucxa;
 
+create table Khunha
+(
+		MaNha int primary key not null,
+        TenNha varchar(20) not null
+);
 
--- Khanh Phong , Thietbi_phong,Khu_nha ,thiet bi 
--- Phong
 create table Phong
 (
-		Ma_Phong int primary key not null,
-        Ten_Phong nvarchar(50) not null,
-        Ma_Nha int not null,
-        Loai_Phong nvarchar(20) not null,
-        So_Nguoi_Toi_Da int not null,
-        So_Nguoi_Dang_O int,
-        Tien_Thue decimal not null,
-        Ghi_Chu text
-);
-create table Thietbi_Phong
-(
-		Ma_Phong int not null,
-        Ma_Thiet_Bi int not null,
-        So_Luong int,
-        Tinh_Trang nvarchar(50) not null,
-        FOREIGN KEY (Ma_Phong) REFERENCES Phong(Ma_Phong),
-        FOREIGN KEY (Ma_Thiet_Bi) REFERENCES Thiet_Bi(Ma_Thiet_Bi)
-);
-create table Khu_nha
-(
-		Ma_Nha int primary key not null,
-        Ten_Nha nvarchar(20) not null
-);
-create table Thiet_Bi
-(
-		Ma_Thiet_Bi int primary key not null,
-        Ten_Thiet_Bi nvarchar(30) not null,
-        Gia_Tri decimal not null 
-);
-create table Account 
-(
-	Account_id int primary key,
-	Username nvarchar(100) not null,
-    Password nvarchar(1000) not null,
-    Account_type int not null default 0 -- 0 la staff va 1 la admin
-);
--- Hai ThutienPhong , traphong 
-create table Thutienphong
-(
-	Ma_phong int not null,
-    Thang int not null,
-    Nam int not null,
-    primary key (Ma_phong, Thang, Nam),
-    Tiennha decimal not null,
-    Tiendien decimal not null,
-    Tiennuoc decimal not null,
-    Tienvesinh decimal not null,
-    Tienphat decimal,
-    Ngayhethan date not null,
-    Ngaydong date not null,
-    foreign key (Ma_phong) references Phong(Ma_phong)
-);
-create table Traphong
-(
-	Masothue int not null,
-    Ngaytra date not null,
-    Tienvipham decimal,
-    foreign key (Masothue) references Thuephong (Masothue)
+		MaPhong int primary key not null,
+        TenPhong varchar(50) not null,
+        MaNha int not null,
+        LoaiPhong varchar(20) not null,
+        SoNguoiToiDa int not null,
+        SoNguoiDangO int,
+        TienThue decimal not null,
+        GhiChu text,
+        foreign key (Manha) references Khunha(Manha)
 );
 
--- Hieu sinh vien, Khoa, lop, thue phong, que
-create table Thuephong
+create table ThietBi
 (
-	Masothue int primary key not null,
-    Masv int not null,
-    Ma_phong int not null,
-    NgayBDau date not null,
-    Ngaykt date not null,
-    Ghichu text,
-    foreign key (Ma_phong) references Phong(Ma_phong),
-    foreign key (Masv) references Sinhvien(Ma_sv)
+		MaThietBi int primary key not null,
+        TenThietBi varchar(30) not null,
+        GiaTri decimal not null 
 );
-create table Sinhvien
+
+create table ThietbiPhong
 (
-	Ma_sv int primary key not null,
-    Ten_sv varchar(50) not null,
-    Ngaysinh int not null,
-    Gioitinh varchar(10) not null,
-    MaQue int not null,
-    MaKhoa int not null,
-    MaLop int not null
+		MaPhong int not null,
+        MaThietBi int not null,
+        SoLuong int,
+        TinhTrang varchar(50) not null,
+        FOREIGN KEY (MaPhong) REFERENCES Phong(MaPhong),
+        FOREIGN KEY (MaThietBi) REFERENCES ThietBi(MaThietBi)
+);
+
+create table Account 
+(
+	AccountId int primary key,
+	Username varchar(100) not null,
+    Password varchar(1000) not null,
+    AccountType int not null default 0 -- 0 la staff va 1 la admin
 );
 
 create table Khoa
@@ -100,11 +56,61 @@ create table Lop
 	Malop int primary key not null,
     Tenlop int not null,
     Makhoa int not null,
-    constraint fk_makhoa foreign key (Makhoa) references Khoa(Makhoa)
+    foreign key (Makhoa) references Khoa(Makhoa)
 );
 
 create table Que
 (
 	Maque int primary key not null,
     Tenque varchar(20) not null
+);
+
+create table Sinhvien
+(
+	Masv int primary key not null,
+    Tensv varchar(50) not null,
+    Ngaysinh int not null,
+    Gioitinh varchar(10) not null,
+    MaQue int not null,
+    MaKhoa int not null,
+    MaLop int not null,
+    foreign key (MaKhoa) references Khoa(MaKhoa),
+    foreign key (MaLop) references Lop(MaLop),
+    foreign key (MaQue) references Que(MaQue)
+);
+
+create table Thuephong
+(
+	Masothue int primary key not null,
+    Masv int not null,
+    Maphong int not null,
+    NgayBDau date not null,
+    Ngaykt date not null,
+    Ghichu text,
+    foreign key (Maphong) references Phong(Maphong),
+    foreign key (Masv) references Sinhvien(Masv)
+);
+
+create table Thutienphong
+(
+	Maphong int not null,
+    Thang int not null,
+    Nam int not null,
+    primary key (Maphong, Thang, Nam),
+    Tiennha decimal not null,
+    Tiendien decimal not null,
+    Tiennuoc decimal not null,
+    Tienvesinh decimal not null,
+    Tienphat decimal,
+    Ngayhethan date not null,
+    Ngaydong date not null,
+    foreign key (Maphong) references Phong(Maphong)
+);
+
+create table Traphong
+(
+	Masothue int not null,
+    Ngaytra date not null,
+    Tienvipham decimal,
+    foreign key (Masothue) references Thuephong (Masothue)
 );
